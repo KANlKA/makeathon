549f0a962bc5cba30157ea9cedbcdb40b4c0c119
KANlKA <155656637+KANlKA@users.noreply.github.com>
2024-01-06 08:25:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+// for the skills,eduction and experience part, when i click anything out of the 3,
+// its supposed to show the contents of tab contents section
+// It removes the "active-tab" class from all tab contents, effectively hiding them.
+// It then gets the element with the ID equal to the provided tabname and adds the 
+// "active-tab" class to that element, making it visible.
 function opentab(tabname) {
-    var tabcontents = document.getElementsByClassName("tab-contents");
-    for (var i = 0; i < tabcontents.length; i++) {
-        tabcontents[i].classList.remove("active-tab");
-    }
-
     var activeTab = document.getElementById(tabname);
-    if (activeTab) {
+
+    // Checking if the clicked tab is already active
+    if (activeTab.classList.contains("active-tab")) {
+        // If yes, remove the "active-tab" class to hide the content
+        activeTab.classList.remove("active-tab");
+    } else {
+        // If no,remove the "active-tab" class from other tabs and add it to the clicked tab
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        for (var i = 0; i < tabcontents.length; i++) {
+            tabcontents[i].classList.remove("active-tab");
+        }
         activeTab.classList.add("active-tab");
     }
 }
+
+// for scrollable behaviour
 document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
